ea3b4cb8b9a76642a9cf2242074f18e2e3d3bbdb
sei-hoan-nguyen <hnguyen@sorensoneng.com>
2025-05-22 08:39:16 -0700

    update gitignore

---
+++ Packages/SCADA_ServerExtension/SCADA_ServerExtensionErrorValue.cs
@@ -0,0 +1,15 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SCADA_ServerExtensionErrorValue.cs" company="Beckhoff Automation GmbH & Co. KG">
+//     Copyright (c) Beckhoff Automation GmbH & Co. KG. All Rights Reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace SCADA_ServerExtension
+{
+    // Contains extension specific error values.
+    public static class SCADA_ServerExtensionErrorValue
+    {
+        public static readonly uint SCADA_ServerExtensionSuccess = 0x0;
+        public static readonly uint SCADA_ServerExtensionFail = 0x1;
+    }
+}
